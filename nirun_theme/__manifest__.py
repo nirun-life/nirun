42ba92950e8fee8ddc2bac3f20004318cfb56a35
Piruin Panichphol <piruin.p@gmail.com>
2021-03-15 10:10:17 +0700

    [ADD] nirun_theme: customized UI for NIRUN

---
+++ nirun_theme/__manifest__.py
@@ -0,0 +1,17 @@
+#  Copyright (c) 2021 Piruin P.
+
+{
+    "name": "Nirun - Theme",
+    "version": "13.0.0.1.0",
+    "development_status": "Alpha",
+    "category": "Healthcare",
+    "author": "Piruin P.",
+    "website": "https://nirun.life/",
+    "license": "LGPL-3",
+    "maintainers": ["piruin"],
+    "depends": ["web"],
+    "data": ["views/webclient_templates.xml"],
+    "application": False,
+    "auto_install": False,
+    "installable": True,
+}
