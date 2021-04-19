c0a1ad70088716d4829eeea86d4c2fc373e9c9a8
Piruin Panichphol <piruin.p@gmail.com>
2021-04-19 19:10:40 +0700

    [ADD] survey_scoring : handle single choice score and enable matrix score

---
+++ survey_scoring/models/__init__.py
@@ -0,0 +1,4 @@
+#  Copyright (c) 2021 Piruin P.
+
+from . import survey_question
+from . import survey_user
---
+++ survey_scoring/models/survey_question.py
@@ -0,0 +1,43 @@
+#  Copyright (c) 2021 Piruin P.
+
+from odoo import api, fields, models
+
+
+class SurveyQuestion(models.Model):
+    _inherit = "survey.question"
+
+    score = fields.Float(compute="_compute_score")
+
+    @api.depends("labels_ids.answer_score")
+    def _compute_score(self):
+        for rec in self:
+            if not rec.labels_ids:
+                rec.score = 0
+            elif rec.question_type == "simple_choice":
+                rec.score = self.max_score_label(rec.labels_ids).answer_score
+            elif rec.question_type == "matrix" and rec.matrix_subtype == "simple":
+                rec.score = self.max_score_label(rec.labels_ids).answer_score * len(
+                    rec.labels_ids_2
+                )
+            elif rec.question_type == "matrix" and rec.matrix_subtype == "multiple":
+                rec.score = rec.sum_answer_score * len(rec.labels_ids_2)
+            else:
+                rec.score = rec.sum_answer_score
+
+    @property
+    def sum_answer_score(self):
+        self.ensure_one()
+        return sum(
+            [
+                answer_score if answer_score > 0 else 0
+                for answer_score in self.mapped("labels_ids.answer_score")
+            ]
+        )
+
+    @api.model
+    def max_score_label(self, labels):
+        if len(labels) == 1:
+            return labels[0]
+        else:
+            m = self.max_score_label(labels[1:])
+            return m if m.answer_score > labels[0].answer_score else labels[0]
