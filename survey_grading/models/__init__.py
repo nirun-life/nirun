0ddc911e8c05b3c4a65df38cce72050cec999d11
Piruin Panichphol <piruin.p@gmail.com>
2021-05-04 13:29:25 +0700

    [ADD] survey_grading : grading survey quiz

---
+++ survey_grading/models/__init__.py
@@ -0,0 +1,5 @@
+#  Copyright (c) 2021 Piruin P.
+
+from . import survey_grade
+from . import survey_survey
+from . import survey_user_input
---
+++ survey_grading/models/survey_grade.py
@@ -0,0 +1,41 @@
+#  Copyright (c) 2021 Piruin P.
+
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
+
+
+class SurveyGrade(models.Model):
+    _name = "survey.grade"
+    _description = "Survey Scoring Grade"
+    _order = "survey_id, high DESC"
+
+    survey_id = fields.Many2one(
+        "survey.survey", index=True, required=True, ondelete="cascade"
+    )
+    passing_score = fields.Float(related="survey_id.passing_score")
+
+    name = fields.Char("Grade", index=True, translate=True)
+    low = fields.Float(
+        "Low (%)", default=0.0, help="Lowest percentage of score (Inclusive)"
+    )
+    high = fields.Float(
+        "High (%)", default=100.0, help="Highest percentage of score (Inclusive)"
+    )
+    passing_grade = fields.Boolean(compute="_compute_passing_grade", default=False)
+
+    def name_get(self):
+        return [(rec.id, "%s [%d-%d]" % (rec.name, rec.low, rec.high)) for rec in self]
+
+    def is_cover(self, value):
+        return self.low <= value <= self.high
+
+    @api.depends("passing_score", "low")
+    def _compute_passing_grade(self):
+        for rec in self:
+            rec.passing_grade = rec.low >= rec.passing_score
+
+    @api.constrains("low", "high")
+    def _validate_low_high(self):
+        for rec in self:
+            if rec.low > rec.high:
+                raise ValidationError(_("low value must not be more than high value"))
