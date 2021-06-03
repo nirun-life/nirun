141e93bafafcdd140f11a5f30caa89cdcf1f4b91
Piruin Panichphol <piruin.p@gmail.com>
2021-06-03 10:13:06 +0700

    [ADD] nirun_careplan_condition: Module to link between careplan and patient's condition

---
+++ nirun_careplan_condition/__manifest__.py
@@ -0,0 +1,17 @@
+#  Copyright (c) 2021 Piruin P.
+
+{
+    "name": "Care Plans - Conditions",
+    "version": "13.0.0.0.0",
+    "development_status": "Alpha",
+    "category": "Healthcare",
+    "author": "Piruin P.",
+    "website": "https://nirun.life/",
+    "license": "LGPL-3",
+    "maintainers": ["piruin"],
+    "depends": ["nirun_careplan", "nirun_condition"],
+    "data": ["views/careplan_activity_views.xml", "views/condition_views.xml"],
+    "application": False,
+    "auto_install": True,
+    "installable": True,
+}
