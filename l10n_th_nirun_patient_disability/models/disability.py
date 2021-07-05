8ee4204644dc09b92faa07baaa7ac13df9592990
Piruin Panichphol <piruin.p@gmail.com>
2021-07-05 13:05:23 +0700

    [ADD] l10n_th_nirun_patient_disability

---
+++ l10n_th_nirun_patient_disability/models/disability.py
@@ -0,0 +1,34 @@
+#  Copyright (c) 2021 Piruin P.
+
+from odoo import api, fields, models
+
+
+class Disability(models.Model):
+    _name = "ni.disability"
+    _description = "Disability Type"
+    _inherit = ["coding.base"]
+
+    patient_ids = fields.Many2many(
+        "ni.patient", "ni_patient_disability_rel", "disability_id", "patient_id"
+    )
+
+    patient_count = fields.Integer(
+        "Total", compute="_compute_patient_count", store=True, compute_sudo=True
+    )
+    patient_male_count = fields.Integer(
+        "Male", compute="_compute_patient_count", store=True, compute_sudo=True
+    )
+    patient_female_count = fields.Integer(
+        "Female", compute="_compute_patient_count", store=True, compute_sudo=True
+    )
+
+    @api.depends("patient_ids")
+    def _compute_patient_count(self):
+        for rec in self:
+            rec.patient_count = len(rec.patient_ids)
+            rec.patient_male_count = len(
+                rec.patient_ids.filtered(lambda p: p.gender == "male")
+            )
+            rec.patient_female_count = len(
+                rec.patient_ids.filtered(lambda p: p.gender == "female")
+            )
