ffb751d13717d8a347cb32a00181cda54d7e8fb8
Piruin Panichphol <piruin.p@gmail.com>
2021-07-27 11:38:41 +0700

    [IMP] nirun_patient_contact: set view priority

--- nirun_patient_contact/__manifest__.py
+++ nirun_patient_contact/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "Patients - Contacts",
-    "version": "13.0.0.1.0",
+    "version": "13.0.0.1.1",
     "development_status": "Alpha",
     "category": "Healthcare",
     "author": "Piruin P.",
