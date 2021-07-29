6c6fd79a61cf559ceff61dbaa509c0859736c161
Piruin Panichphol <piruin.p@gmail.com>
2021-07-29 11:48:53 +0700

    [FIX] nirun_patient: test fail cause by unit_test not loading demo data

--- nirun_patient/tests/test_patient.py
+++ nirun_patient/tests/test_patient.py
@@ -13,12 +13,23 @@ UniqueViolation = errors.lookup("23505")
 
 class TestPatient(TestPatientCommon):
     def test_patient_resource(self):
-        patient = self.env["ni.patient"].with_user(self.patient_admin)
-        form = Form(patient)
-        form.partner_id = self.browse_ref("nirun_patient.partner_mrs_glenda")
+        patients = self.env["ni.patient"].with_user(self.patient_admin)
+        form = Form(patients)
 
+        madam_glenda = self.env["res.partner"].create(
+            {
+                "name": "Glenda J Langlois",
+                "country_id": self.ref("base.us"),
+                "title": self.ref("base.res_partner_title_madam"),
+            }
+        )
+        form.partner_id = madam_glenda
+        form.save()
+
+        form2 = Form(patients)
+        form2.partner_id = madam_glenda
         try:
-            form.save()
+            form2.save()
         except UniqueViolation:
             self.assertEqual(True, True, "Raised UniqueViolation")
         else:
