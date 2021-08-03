01d2eed2f72f3f6ac964186c390680203a0d0625
Piruin Panichphol <piruin.p@gmail.com>
2021-08-03 11:55:49 +0700

    [IMP] nirun_patient_birthdate_estimate: set birthdate to 1 Jan if estimate is checked

--- nirun_patient_birthdate_estimate/models/patient.py
+++ nirun_patient_birthdate_estimate/models/patient.py
@@ -1,6 +1,7 @@
 #  Copyright (c) 2021 Piruin P.
 
-from odoo import fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
 
 
 class Patient(models.Model):
@@ -12,3 +13,17 @@ class Patient(models.Model):
         help="Indicate whether input birthday is estimated value, "
         "Exactly date of birth is unknown.",
     )
+
+    @api.onchange("birthdate_estimate")
+    def onchange_birthdate_estimate(self):
+        if self.birthdate_estimate:
+            self.birthdate = self.birthdate.replace(month=1, day=1)
+
+    @api.constrains("birthdate", "birthdate_estimate")
+    def _check_birthdate(self):
+        for rec in self:
+            if rec.birthdate_estimate and not rec._estimate_birthdate_format():
+                raise UserError(_("Birth date must be 1 January"))
+
+    def _estimate_birthdate_format(self):
+        return self.birthdate.month == 1 and self.birthdate.day == 1
