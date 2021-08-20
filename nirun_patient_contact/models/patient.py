b86889e92b9f49a0b3e66153f2f0304e0daa8d82
Piruin Panichphol <piruin.p@gmail.com>
2021-08-20 12:08:04 +0700

    [IMP] nirun_patient_contact: Add primary and secondary contact

--- nirun_patient_contact/models/patient.py
+++ nirun_patient_contact/models/patient.py
@@ -1,8 +1,23 @@
 #  Copyright (c) 2021 Piruin P.
-from odoo import fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
 
 
 class Patient(models.Model):
     _inherit = "ni.patient"
 
-    contact_ids = fields.One2many(related="partner_id.child_ids", readonly=False)
+    contact_id = fields.Many2one("res.partner", "Primary Contact", ondelete="set null")
+    contact_id_2 = fields.Many2one(
+        "res.partner", "Secondary Contact", ondelete="set null"
+    )
+    contact_ids = fields.One2many(
+        related="partner_id.child_ids", string="Contacts", readonly=False
+    )
+
+    @api.constrains("contact_id", "contact_id_2")
+    def check_contact_id(self):
+        for rec in self:
+            if rec.contact_id == rec.contact_id_2:
+                raise UserError(
+                    _("Primary Contact and secondary should not be same person")
+                )
