7b3dfbf23bcd0d067d7bf3dedc4096fa17a5c340
Piruin Panichphol <piruin.p@gmail.com>
2021-09-27 13:50:07 +0700

    [FIX] l10n_th_nirun_patient: change `country_id` to `nationality_id`

--- l10n_th_nirun_patient/models/patient.py
+++ l10n_th_nirun_patient/models/patient.py
@@ -9,17 +9,17 @@ from odoo.exceptions import UserError
 class Patient(models.Model):
     _inherit = "ni.patient"
 
-    country_id = fields.Many2one(default=lambda self: self.env.ref("base.th"),)
+    nationality_id = fields.Many2one(default=lambda self: self.env.ref("base.th"),)
 
-    @api.onchange("identification_id", "country_id")
+    @api.onchange("identification_id", "nationality_id")
     def _preprocess_identification_id(self):
-        if "TH" == self.country_id.code and self.identification_id:
+        if "TH" == self.nationality_id.code and self.identification_id:
             self.identification_id = re.sub("\\D", "", self.identification_id)
 
-    @api.constrains("identification_id", "country_id")
+    @api.constrains("identification_id", "nationality_id")
     def _check_identification_id(self):
         for record in self:
-            if "TH" == record.country_id.code and record.identification_id:
+            if "TH" == record.nationality_id.code and record.identification_id:
                 _id = record.identification_id
                 if len(_id) != 13 or not _id.isdigit():
                     raise UserError(
