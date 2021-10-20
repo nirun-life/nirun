fe1782e06e5645172db87061a1514340ac3577ca
Piruin Panichphol <piruin.p@gmail.com>
2021-10-20 10:35:25 +0700

    [IMP] nirun_patient_address_location: update follow patient structure change

--- nirun_patient_address_location/models/patient.py
+++ nirun_patient_address_location/models/patient.py
@@ -1,30 +1,32 @@
 #  Copyright (c) 2021 Piruin P.
 
-from odoo import _, api, fields, models
+from odoo import _, api, models
 from odoo.exceptions import ValidationError
 
 
 class Patient(models.Model):
     _inherit = "ni.patient"
 
-    zip_id = fields.Many2one(related="partner_id.zip_id", readonly=False, index=True)
-    city_id = fields.Many2one(
-        related="partner_id.city_id", readonly=False, index=True
-    )  # add index for performance
-
     @api.onchange("city_id")
     def _onchange_city_id(self):
         if not self.zip_id:
-            super()._onchange_city_id()
+            if self.city_id:
+                self.city = self.city_id.name
+                self.zip = self.city_id.zipcode
+                self.state_id = self.city_id.state_id
+            elif self._origin:
+                self.city = False
+                self.zip = False
+                self.state_id = False
         if self.zip_id and self.city_id != self.zip_id.city_id:
             self.update({"zip_id": False, "zip": False, "city": False})
-        if self.city_id and self.partner_id.country_enforce_cities:
+        if self.city_id and self.country_enforce_cities:
             return {"domain": {"zip_id": [("city_id", "=", self.city_id.id)]}}
         return {"domain": {"zip_id": []}}
 
     @api.onchange("country_id")
     def _onchange_country_id(self):
-        res = self.partner_id._onchange_country_id()
+        res = super()._onchange_country_id()
         if self.zip_id and self.zip_id.city_id.country_id != self.country_id:
             self.zip_id = False
         return res
@@ -42,10 +44,18 @@ class Patient(models.Model):
             if self.zip_id.city_id.state_id:
                 vals.update({"state_id": self.zip_id.city_id.state_id})
             self.update(vals)
-        elif not self.partner_id or not self.partner_id.country_enforce_cities:
+        elif not self.country_enforce_cities:
             self.city_id = False
 
-    @api.constrains("zip_id", "country_id", "city_id", "state_id")
+    @api.onchange("state_id")
+    def _onchange_state_id(self):
+        vals = {}
+        if self.state_id.country_id:
+            vals.update({"country_id": self.state_id.country_id})
+        if self.zip_id and self.state_id != self.zip_id.city_id.state_id:
+            vals.update({"zip_id": False, "zip": False, "city": False})
+        self.update(vals)
+
     def _check_zip(self):
         if self.env.context.get("skip_check_zip"):
             return
@@ -71,11 +81,13 @@ class Patient(models.Model):
                     % (rec.name, rec.zip_id.name)
                 )
 
-    @api.onchange("state_id")
-    def _onchange_state_id(self):
-        vals = {}
-        if self.state_id.country_id:
-            vals.update({"country_id": self.state_id.country_id})
-        if self.zip_id and self.state_id != self.zip_id.city_id.state_id:
-            vals.update({"zip_id": False, "zip": False, "city": False})
-        self.update(vals)
+
+class Partner(models.Model):
+    _inherit = "res.partner"
+
+    @api.constrains("zip_id", "country_id", "city_id", "state_id")
+    def _check_zip(self):
+        if self.patient:
+            self.patient_id._check_zip()
+        else:
+            super()._check_zip()
