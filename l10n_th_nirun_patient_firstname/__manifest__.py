aaac2c3ff7034353ae4b76fb18a5861acf542833
Piruin Panichphol <piruin.p@gmail.com>
2021-10-21 15:39:45 +0700

    [ADD] l10n_th_nirun_patient_firstname: Add title into patient form & rearrange naming form into thai traditional

---
+++ l10n_th_nirun_patient_firstname/__manifest__.py
@@ -0,0 +1,17 @@
+#  Copyright (c) 2021 Piruin P.
+
+{
+    "name": "Patients - Name (Thai Localization)",
+    "version": "13.0.0.1.0",
+    "development_status": "Alpha",
+    "category": "Healthcare",
+    "author": "Piruin P.",
+    "website": "https://nirun.life/",
+    "license": "OPL-1",
+    "maintainers": ["piruin"],
+    "depends": ["nirun_patient_firstname", "l10n_th_partner"],
+    "data": ["views/patient_views.xml"],
+    "application": False,
+    "auto_install": False,
+    "installable": True,
+}
---
+++ l10n_th_nirun_patient_firstname/tests/test_form.py
@@ -0,0 +1,35 @@
+#  Copyright (c) 2021 Piruin P.
+
+from odoo.tests.common import Form, TransactionCase
+
+
+class TestL10nThPatientForm(TransactionCase):
+    def setUp(self):
+        super().setUp()
+        self.create_title()
+        self.form = Form(self.env["ni.patient"])
+
+    def create_title(self):
+        self.title = self.env["res.partner.title"].create(
+            {"name": "Mister", "shortcut": "Mr."}
+        )
+
+    def test_patient_name_onchange(self):
+        """Test that you change title"""
+        self.form.lastname = "Panichphol"
+        self.assertEqual(self.form.name, "Panichphol")
+
+        self.form.firstname = "Piruin"
+        self.assertEqual(self.form.name, "Piruin Panichphol")
+
+        self.form.title = self.title
+        self.assertEqual(self.form.name, "Mister Piruin Panichphol")
+
+    def test_patient_name_on_save(self):
+        """Test that you change title"""
+        self.form.title = self.title
+        self.form.lastname = "Panichphol"
+        self.form.firstname = "Piruin"
+        patient = self.form.save()
+
+        self.assertEqual(patient.name, "Mister Piruin Panichphol")
