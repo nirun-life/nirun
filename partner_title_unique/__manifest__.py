849cee9144d0fcceaee598b9f22a2e525f2ab6ba
Piruin Panichphol <piruin.p@gmail.com>
2021-11-02 10:59:47 +0700

    [ADD] partner_title_unique

---
+++ partner_title_unique/__manifest__.py
@@ -0,0 +1,16 @@
+#  Copyright (c) 2021 Piruin P.
+
+{
+    "name": "Partner Title Unique",
+    "version": "13.0.0.1.0",
+    "development_status": "Alpha",
+    "category": "Tools",
+    "author": "Piruin P.",
+    "website": "https://nirun.life/",
+    "license": "LGPL-3",
+    "maintainers": ["piruin"],
+    "depends": ["base"],
+    "application": False,
+    "auto_install": False,
+    "installable": True,
+}
