f4fee32e3ec53c09c62baccf5f86917c070afdaa
Piruin Panichphol <piruin.p@gmail.com>
2021-11-02 10:38:18 +0700

    [IMP] partner_title_order: default sequence=99

--- partner_title_order/models/partner_title.py
+++ partner_title_order/models/partner_title.py
@@ -7,4 +7,4 @@ class PartnerTitle(models.Model):
     _inherit = "res.partner.title"
     _order = "sequence"
 
-    sequence = fields.Integer(index=True, default=0)
+    sequence = fields.Integer(index=True, default=99)
