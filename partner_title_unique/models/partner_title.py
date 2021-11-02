5b3cfd28c11070630c74c830c8145d1a56f53ba7
Piruin Panichphol <piruin.p@gmail.com>
2021-11-02 10:59:47 +0700

    [ADD] partner_title_unique

---
+++ partner_title_unique/models/partner_title.py
@@ -0,0 +1,11 @@
+#  Copyright (c) 2021 Piruin P.
+
+from odoo import _, models
+
+
+class PartnerTitle(models.Model):
+    _inherit = "res.partner.title"
+
+    _sql_constraints = [
+        ("name_unique", "unique (name)", _("This title already exist!"),),
+    ]
