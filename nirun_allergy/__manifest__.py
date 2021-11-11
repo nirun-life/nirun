dc44cd3196ace166a4985c08533b0db6ebf2b399
Piruin Panichphol <piruin.p@gmail.com>
2021-11-11 10:54:57 +0700

    [IMP] nirun_allergy: set priority

--- nirun_allergy/__manifest__.py
+++ nirun_allergy/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "Allergy & Intolerance",
-    "version": "13.0.0.2.0",
+    "version": "13.0.0.2.1",
     "development_status": "Alpha",
     "category": "Healthcare",
     "author": "Piruin P.",
