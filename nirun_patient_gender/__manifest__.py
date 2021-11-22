17564a54ea7dc56db97b2efbbc27c0cfcf8f949e
Piruin Panichphol <piruin.p@gmail.com>
2021-11-22 14:30:09 +0700

    [ADD] nirun_patient_gender

---
+++ nirun_patient_gender/__manifest__.py
@@ -0,0 +1,17 @@
+#  Copyright (c) 2021 Piruin P.
+
+{
+    "name": "Patients - Gender",
+    "summary": "Auto fill patient gender by selected Title",
+    "version": "13.0.0.1.0",
+    "development_status": "Alpha",
+    "category": "Healthcare",
+    "author": "Piruin P.",
+    "website": "https://nirun.life/",
+    "license": "LGPL-3",
+    "maintainers": ["piruin"],
+    "depends": ["nirun_patient", "partner_gender"],
+    "application": False,
+    "auto_install": True,
+    "installable": True,
+}
