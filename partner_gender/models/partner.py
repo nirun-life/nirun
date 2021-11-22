4c4b034c7c2c229931d4e1a68e19ae22f204ae8e
Piruin Panichphol <piruin.p@gmail.com>
2021-11-22 14:28:48 +0700

    [ADD] partner_gender

---
+++ partner_gender/models/partner.py
@@ -0,0 +1,16 @@
+#  Copyright (c) 2021 Piruin P.
+
+from odoo import api, fields, models
+
+
+class Partner(models.Model):
+    _inherit = "res.partner"
+
+    gender = fields.Selection(
+        [("male", "Male"), ("female", "Female"), ("other", "Other")], tracking=True
+    )
+
+    @api.onchange("title")
+    def _onchange_title(self):
+        if self.title and self.title.gender:
+            self.gender = self.title.gender
