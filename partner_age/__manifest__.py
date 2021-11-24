1c537489488fe0eaeed149de5df9f90908ff51bf
Piruin Panichphol <piruin.p@gmail.com>
2021-11-24 16:01:55 +0700

    [ADD] partner_age

---
+++ partner_age/__manifest__.py
@@ -0,0 +1,18 @@
+#  Copyright (c) 2021 Piruin P.
+
+{
+    "name": "Partner Age",
+    "summary": "Add age, birthdate and deceased_date fields for partner",
+    "version": "13.0.0.1.0",
+    "development_status": "Alpha",
+    "category": "Tools",
+    "author": "Piruin P.",
+    "website": "https://nirun.life/",
+    "license": "LGPL-3",
+    "maintainers": ["piruin"],
+    "depends": ["base", "mail"],
+    "data": ["views/partner_views.xml", "data/cron.xml"],
+    "application": False,
+    "auto_install": False,
+    "installable": True,
+}
---
+++ setup/partner_age/setup.py
@@ -0,0 +1,8 @@
+#  Copyright (c) 2021 Piruin P.
+
+import setuptools
+
+setuptools.setup(
+    setup_requires=['setuptools-odoo'],
+    odoo_addon=True,
+)
