a197872a7a662da1afd667379768ea01545e83c7
Piruin Panichphol <piruin.p@gmail.com>
2021-12-02 13:05:40 +0700

    [IMP] nirun_medication: can set pre-define dosage for medication

--- nirun_medication/models/medication_dosage.py
+++ nirun_medication/models/medication_dosage.py
@@ -8,6 +8,8 @@ class Dosage(models.Model):
     _order = "sequence"
 
     sequence = fields.Integer(default=16)
+    name = fields.Char()
+    display_name = fields.Char(compute="_compute_display_name")
     color = fields.Integer()
     text = fields.Text(
         help="How the medication is/was taken or should be taken", tracking=True
@@ -31,9 +33,10 @@ class Dosage(models.Model):
         tracking=True,
     )
     timing_tmpl_id = fields.Many2one("ni.timing.template", store=False)
+    timing_when = fields.Many2many(related="timing_id.when")
 
     site_id = fields.Many2one(
-        "ni.medication.dosage.site", "Body site", help="Body site to administer to"
+        "ni.medication.dosage.site", "Body Site", help="Body site to administer to"
     )
     route_id = fields.Many2one(
         "ni.medication.dosage.route", "Route", help="How drug should enter body"
@@ -45,12 +48,38 @@ class Dosage(models.Model):
     )
     as_need = fields.Boolean("As need?", default=False, tracking=True)
 
+    @api.depends("timing_id", "text", "additional_ids")
+    def _compute_display_name(self):
+        diff = dict(show_text=None, show_additional=None)
+        names = dict(self.with_context(**diff).name_get())
+        for rec in self:
+            rec.display_name = names.get(rec.id)
+
+    def name_get(self):
+        return [(rec.id, rec._name_get()) for rec in self]
+
+    def _name_get(self):
+        rec = self
+        name = rec.timing_id.name
+        if self._context.get("show_text") and self.text:
+            name = "{}\n{}".format(name, rec.text)
+        if self._context.get("show_additional") and self.additional_ids:
+            additional = ", ".join(rec.additional_ids.mapped("name"))
+            name = "{}\n{}".format(name, additional)
+        return name
+
     @api.model
     def create(self, vals):
         if vals.get("timing_tmpl_id") and not vals.get("timing_id"):
             tmpl = self._get_timing_tmpl(vals.get("timing_tmpl_id"))
             vals["timing_id"] = tmpl.to_timing().ids[0]
         return super(Dosage, self).create(vals)
+
+    def update(self, vals):
+        if vals.get("timing_tmpl_id") and not vals.get("timing_id"):
+            tmpl = self._get_timing_tmpl(vals.get("timing_tmpl_id"))
+            vals["timing_id"] = tmpl.to_timing().ids[0]
+        return super(Dosage, self).update(vals)
 
     def _get_timing_tmpl(self, ids):
         return self.env["ni.timing.template"].browse(ids)
