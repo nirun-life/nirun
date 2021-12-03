fc80efc54bea576be47f04fc6cafe5271812d601
Piruin Panichphol <piruin.p@gmail.com>
2021-12-03 11:10:18 +0700

    [ADD] nirun_patient_name_duplicate

---
+++ nirun_patient_name_duplicate/__manifest__.py
@@ -0,0 +1,17 @@
+#  Copyright (c) 2021 Piruin P.
+
+{
+    "name": "Patients - Name Duplication Warning",
+    "summary": "Warning when found patient with same name was already registered",
+    "version": "13.0.0.1.0",
+    "development_status": "Alpha",
+    "category": "Healthcare",
+    "author": "Piruin P.",
+    "website": "https://nirun.life/",
+    "license": "LGPL-3",
+    "maintainers": ["piruin"],
+    "depends": ["nirun_patient", "partner_gender"],
+    "application": False,
+    "auto_install": True,
+    "installable": True,
+}
---
+++ nirun_patient_name_duplicate/models/patient.py
@@ -0,0 +1,28 @@
+#  Copyright (c) 2021 Piruin P.
+
+from odoo import _, api, models
+
+
+class Patient(models.Model):
+    _inherit = "ni.patient"
+
+    @api.onchange("name")
+    def _onchange_name(self):
+        if self.name and self._search_duplicate_name():
+            return {
+                "warning": {
+                    "title": _("Warning! name already exist"),
+                    "message": _(
+                        "Patient with name [%s] was already registered.\n\n"
+                        "Please review there is not the same person."
+                    )
+                    % (self.name),
+                }
+            }
+
+    def _search_duplicate_name(self):
+        self.ensure_one()
+        domain = [("company_id", "=", self.company_id.id), ("name", "=", self.name)]
+        if self.id:
+            domain.append(("id", "!=", self.id[0]))
+        return self.env["ni.patient"].search(domain)
