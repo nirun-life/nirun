7d3f1fd2c40ad24915d8c7eec2c9da510d1cc8e4
Piruin Panichphol <piruin.p@gmail.com>
2022-01-07 15:45:13 +0700

    [IMP] survey_grading: improve grading
    - add badge's color class
    - grade's name in each survey must be unique
    - grade's range must not overlap
    - grade's value must be in between 0.0 - 100

--- survey_grading/models/survey_grade.py
+++ survey_grading/models/survey_grade.py
@@ -19,9 +19,30 @@ class SurveyGrade(models.Model):
         "Low (%)", default=0.0, help="Lowest percentage of score (Inclusive)"
     )
     high = fields.Float(
-        "High (%)", default=100.0, help="Highest percentage of score (Inclusive)"
+        "High (%)",
+        default=100.0,
+        help="Highest percentage of score, "
+        "Inclusive but Exclusive for next lesser grade",
     )
     passing_grade = fields.Boolean(compute="_compute_passing_grade", default=False)
+    color_class = fields.Selection(
+        [
+            ("primary", "Primary"),
+            ("secondary", "secondary"),
+            ("success", "Success"),
+            ("danger", "Danger"),
+            ("warning", "Warning"),
+            ("info", "Info"),
+            ("light", "Light"),
+            ("dark", "Dark"),
+        ],
+        default="primary",
+        help="Bootstrap4's classes to change the appearance of "
+        "this grade's badge at survey result screen",
+    )
+    _sql_constraints = [
+        ("name_uniq", "unique (survey_id, name)", "A grading name must be unique!")
+    ]
 
     def name_get(self):
         return [(rec.id, rec._name_get()) for rec in self]
@@ -34,6 +55,7 @@ class SurveyGrade(models.Model):
         return name
 
     def is_cover(self, value):
+        self.ensure_one()
         return self.low <= value <= self.high
 
     @api.depends("passing_score", "low")
@@ -44,5 +66,25 @@ class SurveyGrade(models.Model):
     @api.constrains("low", "high")
     def _validate_low_high(self):
         for rec in self:
+            if not (0.0 <= rec.low <= 100.0):
+                raise ValidationError(
+                    _("%s low value must be in between 0.0-100.0") % rec.name
+                )
+            if not (0.0 <= rec.high <= 100.0):
+                raise ValidationError(
+                    _("%s high value must be in between 0.0-100.0") % rec.name
+                )
             if rec.low > rec.high:
-                raise ValidationError(_("low value must not be more than high value"))
+                raise ValidationError(
+                    _("%s is not a valid range (%s >= %s)")
+                    % (rec.name, rec.low, rec.high)
+                )
+
+            grade_id = rec.search(
+                [("low", "<", rec.high), ("high", ">", rec.low), ("id", "!=", rec.id)],
+                limit=1,
+            )
+            if grade_id:
+                raise ValidationError(
+                    _("%s is overlapping with %s") % (rec.name, grade_id.name)
+                )
