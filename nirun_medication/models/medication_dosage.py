de028f485ac0ac4ae17bc283a65ee486a0c15984
Piruin Panichphol <piruin.p@gmail.com>
2022-01-25 11:00:14 +0700

    [IMP] nirun_medication: dosage implement ni.timing.mixin

--- nirun_medication/models/medication_dosage.py
+++ nirun_medication/models/medication_dosage.py
@@ -6,6 +6,7 @@ class Dosage(models.Model):
     _name = "ni.medication.dosage"
     _description = "Dosage"
     _order = "sequence"
+    _inherit = ["ni.timing.mixin"]
 
     sequence = fields.Integer(default=16)
     name = fields.Char()
@@ -23,17 +24,6 @@ class Dosage(models.Model):
         help="Supplemental instruction or warnings to the patient - "
         'e.g. "with meals", "may cause drowsiness"',
     )
-    timing_id = fields.Many2one(
-        "ni.timing",
-        "Timing",
-        auto_join=True,
-        index=True,
-        ondelete="cascade",
-        required=True,
-        tracking=True,
-    )
-    timing_tmpl_id = fields.Many2one("ni.timing.template", store=False)
-    timing_when = fields.Many2many(related="timing_id.when")
 
     site_id = fields.Many2one(
         "ni.medication.dosage.site", "Body Site", help="Body site to administer to"
@@ -67,19 +57,3 @@ class Dosage(models.Model):
             additional = ", ".join(rec.additional_ids.mapped("name"))
             name = "{}\n{}".format(name, additional)
         return name
-
-    @api.model
-    def create(self, vals):
-        if vals.get("timing_tmpl_id") and not vals.get("timing_id"):
-            tmpl = self._get_timing_tmpl(vals.get("timing_tmpl_id"))
-            vals["timing_id"] = tmpl.to_timing().ids[0]
-        return super(Dosage, self).create(vals)
-
-    def update(self, vals):
-        if vals.get("timing_tmpl_id") and not vals.get("timing_id"):
-            tmpl = self._get_timing_tmpl(vals.get("timing_tmpl_id"))
-            vals["timing_id"] = tmpl.to_timing().ids[0]
-        return super(Dosage, self).update(vals)
-
-    def _get_timing_tmpl(self, ids):
-        return self.env["ni.timing.template"].browse(ids)
