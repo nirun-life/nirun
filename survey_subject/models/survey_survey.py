f86f918c4c1e2404594170479b9a9bd658f8e065
Piruin Panichphol <piruin.p@gmail.com>
2022-01-27 14:01:00 +0700

    [IMP] survey_subject: add action_survey_subject_wizard
    [IMP] survey_subject: add 'answer survey' button on group by survey_id

--- survey_subject/models/survey_survey.py
+++ survey_subject/models/survey_survey.py
@@ -13,3 +13,12 @@ class Survey(models.Model):
         help="Target that can answer the survey",
         states={"draft": [("readonly", False)]},
     )
+
+    def action_survey_subject_wizard(self):
+        self.ensure_one()
+        ctx = dict(self._context)
+        ctx.update({"default_survey_id": self.id})
+        action = self.env["ir.actions.act_window"].for_xml_id(
+            "survey_subject", "survey_subject_action"
+        )
+        return dict(action, context=ctx)
