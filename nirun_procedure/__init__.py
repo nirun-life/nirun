1c66853c01e615acea01b9c12eb18f825b1e3fcb
Piruin Panichphol <piruin.p@gmail.com>
2022-09-09 10:23:47 +0700

    [ADD] nirun_procedure

---
+++ nirun_procedure/__init__.py
@@ -0,0 +1,3 @@
+#  Copyright (c) 2022. NSTDA
+
+from . import models
---
+++ nirun_procedure/models/__init__.py
@@ -0,0 +1,8 @@
+#  Copyright (c) 2022. NSTDA
+
+from . import encounter
+from . import patient
+from . import procedure
+from . import procedure_category
+from . import procedure_code
+from . import procedure_outcome
