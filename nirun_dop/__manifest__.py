2ad2edd7e1d3fce71122e463278ad2c81c4b2bb6
Piruin Panichphol <piruin.p@gmail.com>
2022-12-15 18:05:56 +0700

    [IMP] nirun_dop:

---
+++ nirun_dop/__manifest__.py
@@ -0,0 +1,20 @@
+#  Copyright (c) 2021-2022. NSTDA
+
+{
+    "name": "Nirun - DOP",
+    "version": "13.0.0.1.0",
+    "development_status": "Alpha",
+    "category": "Healthcare",
+    "author": "NSTDA, Piruin P.",
+    "website": "https://nirun.life/",
+    "license": "OPL-1",
+    "maintainers": ["piruin"],
+    "depends": ["nirun_patient", "nirun_careplan", "nirun"],
+    "data": [
+        # "views/patient_views.xml",
+        "views/encounter_views.xml"
+    ],
+    "application": False,
+    "auto_install": False,
+    "installable": True,
+}
---
+++ nirun_dop/models/__init__.py
@@ -0,0 +1,2 @@
+#  Copyright (c) 2022. NSTDA
+from . import encounter
