3f1de200bbcfb51a57fe05d6fe3ca9d9e10ec12e
Piruin Panichphol <piruin.p@gmail.com>
2022-12-16 17:17:26 +0700

    [IMP] nirun_dop: more hide

--- nirun_dop/models/encounter.py
+++ nirun_dop/models/encounter.py
@@ -7,6 +7,7 @@ class Encounter(models.Model):
     _order = "period_start DESC,name DESC"
 
     priority = fields.Selection(groups="base.group_no_one")
+    pre_admit_identifier = fields.Char(groups="base.group_no_one")
 
     def action_dummy(self):
         return {}
