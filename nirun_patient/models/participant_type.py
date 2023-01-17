8d79664bbecb032e753ffebaf62f4652e177c737
Piruin Panichphol <piruin.p@gmail.com>
2023-01-17 17:03:26 +0700

    [IMP] nirun_patient: better `show_age`

--- nirun_patient/models/participant_type.py
+++ nirun_patient/models/participant_type.py
@@ -1,10 +1,11 @@
-#  Copyright (c) 2022. NSTDA
+#  Copyright (c) 2022-2023. NSTDA
 
 from odoo import fields, models
 
 
 class ParticipantType(models.Model):
     _name = "ni.participant.type"
+    _description = "Participant Type"
     _inherit = "coding.base"
 
     limit = fields.Integer(default=-1)
