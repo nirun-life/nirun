c202f6e0390f59c73cb9b695b1641baecee9a8fa
Piruin Panichphol <piruin.p@gmail.com>
2023-04-12 17:32:06 +0700

    [ADD] ni_coding:
    [IMP] ni_coding: use `color_picker`
    
    [IMP] ni_coding: `Medical` category

---
+++ ni_coding/__manifest__.py
@@ -0,0 +1,16 @@
+#  Copyright (c) 2021-2023 NSTDA
+
+{
+    "name": "Nirun - Coding",
+    "version": "16.0.0.1.0",
+    "development_status": "Alpha",
+    "category": "Medical",
+    "author": "NSTDA, Piruin P.",
+    "website": "https://nirun.life/",
+    "license": "LGPL-3",
+    "maintainers": ["piruin"],
+    "depends": ["base", "mail"],
+    "application": False,
+    "auto_install": False,
+    "installable": True,
+}
---
+++ ni_coding/models/ni_coding.py
@@ -0,0 +1,76 @@
+#  Copyright (c) 2021-2023 NSTDA
+
+import random
+
+from odoo import api, fields, models
+
+
+class Coding(models.AbstractModel):
+    _name = "ni.coding"
+    _description = "Coding"
+    _order = "sequence, id"
+
+    def _get_default_sequence(self):
+        last_sequence = self.env[self._name].search([], order="sequence desc", limit=1)
+        return last_sequence.sequence + 1 if last_sequence else 0
+
+    sequence = fields.Integer(
+        index=True,
+        default=lambda self: self._get_default_sequence(),
+    )
+    name = fields.Char(required=True, index=True, translate=True)
+    code = fields.Char(index=True, size=16)
+    definition = fields.Text(translate=True)
+    color = fields.Integer(default=lambda _: random.randint(0, 10))
+    active = fields.Boolean(default=True)
+
+    @api.model
+    def _name_search(
+        self, name, args=None, operator="ilike", limit=100, name_get_uid=None
+    ):
+        args = list(args or [])
+        if not (name == "" and operator == "ilike"):
+            args += ["|", ("name", operator, name), ("code", operator, name)]
+        return self._search(args, limit=limit, access_rights_uid=name_get_uid)
+
+    def name_get(self):
+        return [(code.id, code._get_name()) for code in self]
+
+    def _get_name(self):
+        coding = self
+        name = coding.name
+        if (
+            self._context.get("show_parent")
+            and "parent_id" in self._fields
+            and coding._fields["parent_id"]
+        ):
+            names = []
+            current = coding
+            while current:
+                names.append(current.name)
+                current = current.parent_id
+            name = ", ".join(reversed(names))
+        if self._context.get("show_code") and self.code:
+            name = "[{}] {}".format(coding.code, name)
+        return name
+
+    _sql_constraints = [
+        (
+            "name__uniq",
+            "unique (name)",
+            "This name already exists!",
+        ),
+        (
+            "code__uniq",
+            "unique (code)",
+            "This code already exists!",
+        ),
+    ]
+
+    def copy(self, default=None):
+        default = default or {}
+        if "name" not in default:
+            default["name"] = "%s (copy)" % self.name
+        if "code" not in default:
+            default["code"] = "%s (copy)" % self.code
+        return super(Coding, self).copy(default)
