78190df3202c7075b264ebf9df65a7f243457abd
Piruin Panichphol <piruin.p@gmail.com>
2023-04-18 14:43:57 +0700

    [MIG] ni_patient_address: version 16.0

---
+++ ni_patient_address/__manifest__.py
@@ -0,0 +1,20 @@
+#  Copyright (c) 2021-2023 NSTDA
+
+{
+    "name": "Patients - Address",
+    "version": "16.0.0.1.0",
+    "development_status": "Alpha",
+    "category": "Healthcare",
+    "author": "NSTDA, Piruin P.",
+    "website": "https://nirun.life/",
+    "license": "OPL-1",
+    "maintainers": ["piruin"],
+    "depends": ["ni_patient"],
+    "data": [
+        "views/ni_patient_views.xml",
+        "views/ni_encounter_views.xml",
+    ],
+    "application": False,
+    "auto_install": False,
+    "installable": True,
+}
---
+++ ni_patient_address/models/__init__.py
@@ -0,0 +1,4 @@
+#  Copyright (c) 2023 NSTDA
+
+from . import ni_patient
+from . import ni_encounter
