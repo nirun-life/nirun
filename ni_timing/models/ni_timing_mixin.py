f13111f2f0d33c54d5881e36c1925f1a568b296c
Piruin Panichphol <piruin.p@gmail.com>
2023-05-01 16:52:36 +0700

    [IMP] ni_timing: implement create in batch

--- ni_timing/models/ni_timing_mixin.py
+++ ni_timing/models/ni_timing_mixin.py
@@ -11,7 +11,6 @@ class TimingMixin(models.AbstractModel):
         "ni.timing.timing",
         auto_join=True,
         ondelete="set null",
-        tracking=True,
         domain=[
             ("res_model", "=", lambda self: self._name),
             ("res_id", "=", lambda self: self.id),
@@ -22,17 +21,18 @@ class TimingMixin(models.AbstractModel):
     timing_dow = fields.Many2many(related="timing_id.day_of_week")
     timing_tod = fields.One2many(related="timing_id.time_of_day")
 
-    @api.model
-    def create(self, vals):
-        if vals.get("timing_tmpl_id") and not vals.get("timing_id"):
-            tmpl = self._get_timing_tmpl(vals.get("timing_tmpl_id"))
-            vals["timing_id"] = tmpl.to_timing().ids[0]
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if vals.get("timing_tmpl_id") and not vals.get("timing_id"):
+                tmpl = self._get_timing_tmpl(vals.get("timing_tmpl_id"))
+                vals["timing_id"] = tmpl.to_timing().ids[0]
 
-        record = super(TimingMixin, self).create(vals)
-
-        if record.timing_id:
-            record.timing_id.write({"res_model": record._name, "res_id": record.id})
-        return record
+        records = super(TimingMixin, self).create(vals_list)
+        for rec in records:
+            if rec.timing_id:
+                rec.timing_id.write({"res_model": rec._name, "res_id": rec.id})
+        return records
 
     def write(self, vals):
         timing_tmpl = vals.get("timing_tmpl_id") and not vals.get("timing_id")
