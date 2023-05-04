3ab483c4005f66eb223bf07a0071051a36613d1a
Piruin Panichphol <piruin.p@gmail.com>
2023-05-04 15:14:06 +0700

    [MIG] l10n_th_web_font_kanit: version 16.0

---
+++ l10n_th_web_font_kanit/__manifest__.py
@@ -0,0 +1,22 @@
+#  Copyright (c) 2023 NSTDA
+
+{
+    "name": "Kanit Font",
+    "summary": "Kanit Font for Odoo",
+    "version": "16.0.0.1.0",
+    "development_status": "Alpha",
+    "category": "Website",
+    "author": "NSTDA, Piruin P.",
+    "website": "https://nirun.life/",
+    "license": "LGPL-3",
+    "maintainers": ["piruin"],
+    "depends": ["web"],
+    "assets": {
+        "web.assets_common": [
+            "/l10n_th_web_font_kanit/static/src/scss/fonts.scss",
+        ],
+    },
+    "application": False,
+    "auto_install": False,
+    "installable": True,
+}
