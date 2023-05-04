bfc2832b0c3df162c8b0e4a26b27b76f5da30ab9
Piruin Panichphol <piruin.p@gmail.com>
2023-05-04 14:51:20 +0700

    [MIG] web_enterprise_theme: version 16.0

---
+++ web_enterprise_theme/__manifest__.py
@@ -0,0 +1,29 @@
+#  Copyright (c) 2023 NSTDA
+
+{
+    "name": "Enterprise Theme",
+    "summary": "Odoo Enterprise Theme",
+    "version": "16.0.0.1.0",
+    "development_status": "Alpha",
+    "category": "Website",
+    "author": "NSTDA, Piruin P.",
+    "website": "https://nirun.life/",
+    "license": "LGPL-3",
+    "maintainers": ["piruin"],
+    "depends": ["web"],
+    "assets": {
+        "web._assets_primary_variables": [
+            (
+                "after",
+                "web/static/src/scss/primary_variables.scss",
+                "/web_enterprise_theme/static/src/scss/_assets_primary_variables.scss",
+            ),
+        ],
+        "web.assets_backend": [
+            "/web_enterprise_theme/static/src/scss/assets_backend.scss",
+        ],
+    },
+    "application": False,
+    "auto_install": False,
+    "installable": True,
+}
