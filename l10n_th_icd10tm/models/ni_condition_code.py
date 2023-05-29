441a534d1101986fb20c3acb2638d96351fe10e0
Piruin Panichphol <piruin.p@gmail.com>
2023-05-29 15:34:00 +0700

    [IMP] l10n_th_idc10tm: add code_simplify for ni.condition.code

--- l10n_th_icd10tm/models/ni_condition_code.py
+++ l10n_th_icd10tm/models/ni_condition_code.py
@@ -1,11 +1,14 @@
 #  Copyright (c) 2021-2023 NSTDA
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ConditionCode(models.Model):
     _inherit = "ni.condition.code"
 
+    code_simplify = fields.Char(
+        compute="_compute_code_simplify", store=True, index=True
+    )
     chapter_id = fields.Many2one(
         "ni.condition.chapter", related="block_id.chapter_id", store=True
     )
@@ -14,3 +17,24 @@ class ConditionCode(models.Model):
     type = fields.Selection(
         [("code", "Code"), ("header", "Header")], default="code", required=True
     )
+
+    @api.model
+    def _name_search(
+        self, name, args=None, operator="ilike", limit=100, name_get_uid=None
+    ):
+        args = list(args or [])
+        if not (name == "" and operator == "ilike"):
+            args += [
+                "|",
+                "|",
+                ("name", operator, name),
+                ("code", operator, name),
+                ("code_simplify", operator, name),
+            ]
+        return self._search(args, limit=limit, access_rights_uid=name_get_uid)
+
+    @api.depends("code")
+    def _compute_code_simplify(self):
+        for rec in self:
+            if rec.code:
+                rec.code_simplify = rec.code.replace(".", "")
