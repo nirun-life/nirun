7bdf534cafb51b71315d78e519b260dccde611b0
Piruin Panichphol <piruin.p@gmail.com>
2023-05-30 14:42:54 +0700

    [IMP] ni_condition: Dx Role can have parent

--- ni_condition/models/ni_encounter_diagnosis_role.py
+++ ni_condition/models/ni_encounter_diagnosis_role.py
@@ -1,12 +1,13 @@
 #  Copyright (c) 2023 NSTDA
 
-from odoo import fields, models
+from odoo import _, api, fields, models
 
 
 class DiagnosisRole(models.Model):
     _name = "ni.encounter.diagnosis.role"
     _description = "Diagnosis Role"
     _inherit = ["ni.coding"]
+    _parent_store = True
 
     limit = fields.Integer(default=-1)
     decoration = fields.Selection(
@@ -21,3 +22,12 @@ class DiagnosisRole(models.Model):
         default="muted",
         required=True,
     )
+    parent_id = fields.Many2one(
+        "ni.encounter.diagnosis.role", index=True, ondelete="cascade"
+    )
+    parent_path = fields.Char(index=True, unaccent=False)
+
+    @api.constrains("parent_id")
+    def _check_parent_id(self):
+        if not self._check_recursion():
+            raise models.ValidationError(_("Error! You cannot create recursive item."))
