fef2ea397ecb7c18f47f37dfa984f6f57030614f
Piruin Panichphol <piruin.p@gmail.com>
2023-06-20 14:56:57 +0700

    [IMP] ni_document_ref: add document count

--- ni_document_ref/models/ni_encounter.py
+++ ni_document_ref/models/ni_encounter.py
@@ -1,6 +1,6 @@
 #  Copyright (c) 2022-2023 NSTDA
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 from odoo.addons.ni_patient.models.ni_encounter import LOCK_STATE_DICT
 
@@ -11,3 +11,9 @@ class Encounter(models.Model):
     document_ids = fields.One2many(
         "ni.document.ref", "encounter_id", states=LOCK_STATE_DICT
     )
+    document_count = fields.Integer(compute="_compute_document_count")
+
+    @api.depends("document_ids")
+    def _compute_document_count(self):
+        for rec in self:
+            rec.document_count = len(rec.document_ids)
