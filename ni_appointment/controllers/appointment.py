5286869844b855053fd1ebef54166febc3028fd3
harit <haritsug@scg.com>
2023-07-24 21:38:35 +0700

    load appointment list

--- ni_appointment/controllers/appointment.py
+++ ni_appointment/controllers/appointment.py
@@ -7,7 +7,34 @@ from odoo.addons.portal.controllers.portal import CustomerPortal
 
 
 class AppointmentPortal(CustomerPortal):
+    def _prepare_home_portal_values(self, counters):
+        values = super()._prepare_home_portal_values(counters)
+        if "appointment_count" in counters:
+            values["appointment_count"] = (
+                request.env["ni.appointment"].search_count(
+                    [("partner_ids", "=", request.env.user.partner_id.id)]
+                )
+                if request.env["ni.appointment"].check_access_rights(
+                    "read", raise_exception=False
+                )
+                else 0
+            )
+        return values
+
     @http.route(["/my/appointment"], type="http", auth="user", website=True)
     def portal_my_appointment(self, **kw):
+        values = self._prepare_portal_layout_values()
+        Appointment = request.env["ni.appointment"]
+        domain = [("partner_ids", "=", request.env.user.partner_id.id)]
 
-        return request.render("ni_appointment.portal_my_appointments", {})
+        appointments = Appointment.search(domain)
+
+        values.update(
+            {
+                "appointments": appointments,
+                "page_name": "appointment",
+                "default_url": "/my/appointment",
+            }
+        )
+
+        return request.render("ni_appointment.portal_my_appointments", values)
