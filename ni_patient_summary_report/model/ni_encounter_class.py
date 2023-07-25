741d35fb6d92f02170bc8a6b351c9c7a8871d542
kawinman <80952798+kawinman@users.noreply.github.com>
2023-07-25 12:03:53 +0700

    [ADD] ni_patient_summary_report
    Co-authored-by: harit <haritsug@scg.com>
    Co-authored-by: kawinman <KAWINMAN@SCG.COM>

---
+++ ni_patient_summary_report/model/ni_encounter_class.py
@@ -0,0 +1,18 @@
+#  Copyright (c) 2023 NSTDA
+from odoo import fields, models
+
+
+class EncounterClass(models.Model):
+    _inherit = "ni.encounter.class"
+
+    summary_report_title = fields.Char()
+
+    def action_print_summary_report(self):
+        return self.env.ref(
+            "ni_patient_summary_report.action_report_summary"
+        ).report_action(self.ids)
+
+    def action_print_medical_certificate(self):
+        return self.env.ref(
+            "ni_patient_summary_report.action_report_medical_certificate"
+        ).report_action(self.ids)
