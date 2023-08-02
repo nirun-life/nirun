e2b5502ad6585d8421cee3a42bd3beced0ff2330
Piruin Panichphol <piruin.p@gmail.com>
2023-08-02 15:06:27 +0700

    [IMP] ni_patient_summary_report: can set medication certification append

---
+++ ni_patient_summary_report/model/res_company.py
@@ -0,0 +1,19 @@
+#  Copyright (c) 2023 NSTDA
+
+from odoo import _, fields, models
+
+APPEND = _(
+    """
+<h5>Recommendation</h5>
+<ul class=""o_checklist"">
+    <li id=""checkId-1"">Patient need rest for ........................ day</li>
+    <li id=""checkId-2"">Patient has received treatment in the hospital</li>
+</ul>
+"""
+)
+
+
+class Company(models.Model):
+    _inherit = "res.company"
+
+    med_cert_append = fields.Html("Append", default=APPEND, translate=True)
