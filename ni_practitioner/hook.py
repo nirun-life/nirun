4b9307abbee1d88e76f500bec931a37c44099f90
Piruin Panichphol <piruin.p@gmail.com>
2023-08-10 22:12:27 +0700

    [FIX] ni_practitioner: check if active column already exist

--- ni_practitioner/hook.py
+++ ni_practitioner/hook.py
@@ -5,5 +5,8 @@ def pre_init_hook(cr):
     """
     We need to manual alter table before odoo automate that will failre
     """
-    sql = """ALTER TABLE hr_employee_category ADD COLUMN active BOOLEAN NOT NULL DEFAULT TRUE;"""
+    sql = """
+    ALTER TABLE IF EXISTS hr_employee_category
+    ADD COLUMN IF NOT EXISTS active BOOLEAN NOT NULL DEFAULT TRUE;
+    """
     cr.execute(sql)
