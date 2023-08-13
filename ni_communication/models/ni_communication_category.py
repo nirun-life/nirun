283a01b5dc6c41767b42291fe489837edc220100
Piruin Panichphol <piruin.p@gmail.com>
2023-08-13 15:33:56 +0700

    [IMP] ni_communication: Add content page for category

--- ni_communication/models/ni_communication_category.py
+++ ni_communication/models/ni_communication_category.py
@@ -1,9 +1,11 @@
 #  Copyright (c) 2023. NSTDA
 
-from odoo import models
+from odoo import fields, models
 
 
 class CommunicationCategory(models.Model):
     _name = "ni.communication.category"
     _description = "Communication Category"
     _inherit = ["ni.coding"]
+
+    content_ids = fields.One2many("ni.communication.content", "category_id")
