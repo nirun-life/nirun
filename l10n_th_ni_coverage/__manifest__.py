fd27eb041ca9a5254d219933df7db63c460430d8
Piruin Panichphol <piruin.p@gmail.com>
2023-10-06 09:50:06 +0700

    [ADD] l10n_th_ni_coverage

---
+++ l10n_th_ni_coverage/__manifest__.py
@@ -0,0 +1,19 @@
+#  Copyright (c) 2021-2023 NSTDA
+
+{
+    "name": "Thai Coverage",
+    "version": "16.0.0.1.0",
+    "development_status": "Alpha",
+    "category": "Medical",
+    "author": "NSTDA, Piruin P.",
+    "website": "https://nirun.life/",
+    "license": "LGPL-3",
+    "maintainers": ["piruin"],
+    "depends": [
+        "ni_coverage",
+    ],
+    "data": ["data/ni.coverage.type.csv"],
+    "application": False,
+    "auto_install": False,
+    "installable": True,
+}
