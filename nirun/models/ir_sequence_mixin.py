c64c5423b2035717795f88a698c97dbaac4b4850
Piruin Panichphol <piruin.p@gmail.com>
2023-11-22 17:15:31 +0700

    [FIX] nirun: ir.sequence.mixin not work when create multi

--- nirun/models/ir_sequence_mixin.py
+++ nirun/models/ir_sequence_mixin.py
@@ -11,29 +11,29 @@ class IrSequenceMixin(models.AbstractModel):
     _sequence_field = "name"
     _sequence_ts_field = "period_start"
 
-    @api.model
-    def create(self, vals):
-        if (
-            vals.get(self._sequence_field, self._sequence_default)
-            == self._sequence_default
-        ):
-            seq_date = fields.Date.today()
-            seq = self.env["ir.sequence"]
-            if self._sequence_ts_field in vals:
-                seq_date = fields.Datetime.context_timestamp(
-                    self, fields.Datetime.to_datetime(vals[self._sequence_ts_field])
-                )
-            if "company_id" in vals:
-                vals[self._sequence_field] = (
-                    seq.with_context(force_company=vals["company_id"]).next_by_code(
-                        self._name, sequence_date=seq_date
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if (
+                vals.get(self._sequence_field, self._sequence_default)
+                == self._sequence_default
+            ):
+                seq_date = fields.Date.today()
+                seq = self.env["ir.sequence"]
+                if self._sequence_ts_field in vals:
+                    seq_date = fields.Datetime.context_timestamp(
+                        self, fields.Datetime.to_datetime(vals[self._sequence_ts_field])
                     )
-                    or self._sequence_default
-                )
-            else:
-                vals[self._sequence_field] = (
-                    seq.next_by_code(self._name, sequence_date=seq_date)
-                    or self._sequence_default
-                )
-
-        return super().create(vals)
+                if "company_id" in vals:
+                    vals[self._sequence_field] = (
+                        seq.with_context(force_company=vals["company_id"]).next_by_code(
+                            self._name, sequence_date=seq_date
+                        )
+                        or self._sequence_default
+                    )
+                else:
+                    vals[self._sequence_field] = (
+                        seq.next_by_code(self._name, sequence_date=seq_date)
+                        or self._sequence_default
+                    )
+        return super().create(vals_list)
