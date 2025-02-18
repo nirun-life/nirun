b6e57a998ba4c52b7d08332f77c9059cdd305bfc
Piruin P. <piruin.p@gmail.com>
2025-02-18 11:45:00 +0700

    [ADD] base_location_private_form

---
+++ base_location_private_form/__manifest__.py
@@ -0,0 +1,19 @@
+#  Copyright (c) 2023 NSTDA
+
+{
+    "name": "Base Location Partner Private Form",
+    "version": "16.0.0.1.0",
+    "development_status": "Alpha",
+    "category": "Medical",
+    "author": "NSTDA, Piruin P.",
+    "website": "https://nirun.life/",
+    "license": "LGPL-3",
+    "maintainers": ["piruin"],
+    "depends": ["base", "base_location"],
+    "data": [
+        "views/res_partner_view.xml",
+    ],
+    "application": False,
+    "auto_install": False,
+    "installable": True,
+}
