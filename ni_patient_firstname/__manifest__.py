309c12f10b5dd8a89b0a4c9cf5331cc6c5e100ad
KAWINMAN <KAWINMAN@SCG.COM>
2025-03-02 12:16:05 +0700

    [ADD] ni_patient_firstname

---
+++ ni_patient_firstname/__manifest__.py
@@ -0,0 +1,19 @@
+#  Copyright (c) 2021-2023 NSTDA
+
+{
+    "name": "Patients - Firstname and Lastname",
+    "version": "16.0.0.1.0",
+    "development_status": "Alpha",
+    "category": "Medical",
+    "author": "NSTDA, Piruin P.",
+    "website": "https://nirun.life/",
+    "license": "LGPL-3",
+    "maintainers": ["piruin"],
+    "depends": ["ni_patient", "partner_firstname"],
+    "data": [
+        "views/ni_patient_views.xml",
+    ],
+    "application": False,
+    "auto_install": True,
+    "installable": True,
+}
