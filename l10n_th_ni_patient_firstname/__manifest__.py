dc9daae0e087e63577b2bc8443821cc16d2cbdd5
KAWINMAN <KAWINMAN@SCG.COM>
2025-03-02 12:59:43 +0700

    [ADD] l10n_th_ni_patient_firstname

---
+++ l10n_th_ni_patient_firstname/__manifest__.py
@@ -0,0 +1,17 @@
+#  Copyright (c) 2021 NSTDA
+
+{
+    "name": "Patients Thai Localization - Firstname and Lastname",
+    "version": "16.0.0.1.0",
+    "development_status": "Alpha",
+    "category": "Healthcare",
+    "author": "NSTDA, Piruin P.",
+    "website": "https://nirun.life/",
+    "license": "OPL-1",
+    "maintainers": ["piruin"],
+    "depends": ["ni_patient_firstname"],
+    "data": ["views/ni_patient_views.xml"],
+    "application": False,
+    "auto_install": False,
+    "installable": True,
+}
