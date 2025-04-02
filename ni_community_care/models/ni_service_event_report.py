2fc9680fc843ead62ef87c39a74139152e240ab8
Piruin Panichphol <piruin.p@gmail.com>
2025-04-02 10:03:13 +0700

    [IMP] ni_community_care: dashboard show only user's event

--- ni_community_care/models/ni_service_event_report.py
+++ ni_community_care/models/ni_service_event_report.py
@@ -7,15 +7,21 @@ from odoo import _, api, fields, models, tools
 
 class ServiceEventReport(models.Model):
     _name = "ni.service.event.report"
-    _description = "Event Report"
+    _description = "Service Event Report"
     _auto = False
 
-    event_id = fields.Many2one("ni.service.event")
+    name = fields.Char("ชื่อกิจกรรม")
+    event_id = fields.Many2one("calendar.event")
     start = fields.Datetime()
     stop = fields.Datetime()
-    duration = fields.Float()
-    patient_id = fields.Many2one("ni.patient")
-    patient_type_id = fields.Many2one("ni.patient.type")
+    duration = fields.Float("ระยะเวลา")
+    service_id = fields.Many2one("ni.service", "กิจกรรม")
+    service_ids = fields.Many2many(
+        "ni.service", "ni_service_event_rel", "event_id", "service_id", "กิจกรรม"
+    )
+    patient_id = fields.Many2one("ni.patient", "ผู้สูงอายุ")
+    patient_type_id = fields.Many2one("ni.patient.type", "ประเภทผู้สูงอายุ")
+    user_id = fields.Many2one("res.users", "บัญชีผู้บริบาล")
 
     def init(self):
         tools.drop_view_if_exists(self.env.cr, self._table)
@@ -24,13 +30,17 @@ class ServiceEventReport(models.Model):
             SELECT
                 pe.ni_service_event_id as id,
                 se.id as event_id,
+                c.name,
+                se.service_id,
                 se.start,
                 se.stop,
                 se.duration,
                 pat.type_id as patient_type_id,
-                pe.ni_patient_id as patient_id
+                pe.ni_patient_id as patient_id,
+                c.user_id
             FROM ni_patient_ni_service_event_rel pe
             LEFT JOIN ni_service_event se ON pe.ni_service_event_id = se.id
+            LEFT JOIN calendar_event c ON se.event_id = c.id
             LEFT JOIN ni_patient pat ON pe.ni_patient_id = pat.id
             WHERE se.patient_type_id IS NOT NULL
         )
@@ -74,7 +84,11 @@ class ServiceEventReport(models.Model):
                 res[rec.code]["target"] = rec.target
 
         datas = self.read_group(
-            [("start", ">=", this_month), ("start", "<", next_month)],
+            [
+                ("start", ">=", this_month),
+                ("start", "<", next_month),
+                ("user_id", "=", self.env.user.id),
+            ],
             ["patient_type_id", "patient_id:count_distinct"],
             ["patient_type_id"],
         )
