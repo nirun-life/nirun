2adfcf06b6da2d03c206abba575b50c4244fa78d
Piruin P. <piruin.p@gmail.com>
2025-04-22 11:46:00 +0700

    [ADD] website_hide_signin

---
+++ website_hide_signin/__manifest__.py
@@ -0,0 +1,18 @@
+#  Copyright (c) 2023 NSTDA
+
+{
+    "name": "Website - Hide Sign-in button",
+    "summary": "Hide Sign-in button on front-end website",
+    "version": "16.0.0.1.0",
+    "development_status": "Alpha",
+    "category": "Website",
+    "author": "NSTDA, piruin",
+    "website": "https://nirun.life/",
+    "license": "LGPL-3",
+    "maintainers": ["piruin"],
+    "depends": ["website"],
+    "data": ["views/website_templates.xml"],
+    "application": False,
+    "auto_install": False,
+    "installable": True,
+}
