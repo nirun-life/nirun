3a528bfee5857ecefdd98024d824c939bc6275dc
Piruin Panichphol <piruin.p@gmail.com>
2025-05-30 17:40:48 +0700

    [IMP] ni_careplan: add service template

--- ni_careplan/models/ni_careplan_category.py
+++ ni_careplan/models/ni_careplan_category.py
@@ -20,8 +20,48 @@ class CareplanCategory(models.Model):
     service_category_id = fields.Many2one(
         "ni.service.category", help="Default service category"
     )
+    service_request_ids = fields.One2many("ni.careplan.template.service", "template_id")
 
     @api.constrains("parent_id")
     def _check_parent_id(self):
         if not self._check_recursion():
             raise models.ValidationError(_("Error! You cannot create recursive data."))
+
+
+class ServiceTemplate(models.Model):
+    _name = "ni.careplan.template.service"
+    _inherit = "ni.timing.mixin"
+
+    company_id = fields.Many2one("res.company", copy=False)
+    template_id = fields.Many2one("ni.careplan.category", copy=False)
+    name = fields.Char("Service Name", required=True)
+    category_id = fields.Many2one(
+        "ni.service.category",
+        domain=lambda self: [
+            ("id", "!=", self.env.ref("ni_service.categ_routine").id),
+        ],
+    )
+    service_ids = fields.Many2many(
+        "ni.service",
+        "ni_careplan_template_service_rel",
+        "template_id",
+        "service_id",
+        check_company=True,
+    )
+
+    def _default_service_domain(self):
+        return [("category_id", "!=", self.env.ref("ni_service.categ_routine").id)]
+
+    @api.onchange("category_id")
+    def _onchange_category_id(self):
+        if self.category_id:
+            domain = [("category_id", "=", self.category_id.id)]
+        else:
+            domain = self._default_service_domain()
+        return {"domain": {"service_ids": domain}}
+
+    @api.onchange("service_ids")
+    def _onchange_service_ids(self):
+        for rec in self:
+            if rec.service_ids:
+                rec.name = ", ".join(rec.service_ids.mapped("name"))
