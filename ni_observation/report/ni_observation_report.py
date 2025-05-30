5e682d0da22f268abd44cceabd5361be535936e6
Piruin P. <piruin.p@gmail.com>
2025-05-30 13:27:58 +0700

    [IMP] ni_observation: show history of observation

---
+++ ni_observation/report/ni_observation_report.py
@@ -0,0 +1,55 @@
+#  Copyright (c) 2025 NSTDA
+from odoo import fields, models
+
+
+class ObservationReport(models.AbstractModel):
+    _name = "ni.observation.report"
+    _description = "Observation Report Abstract"
+    _auto = False
+    _order = "occurrence desc"
+    _inherit = ["ni.observation.abstract"]
+
+    sheet_id = fields.Many2one("ni.observation.sheet", readonly=True)
+    company_id = fields.Many2one("res.company", readonly=True)
+    patient_id = fields.Many2one("ni.patient")
+    encounter_id = fields.Many2one("ni.encounter")
+    occurrence = fields.Datetime(readonly=True)
+    display_type = fields.Selection(
+        [("line_section", "Section"), ("line_note", "Note")]
+    )
+    type_id = fields.Many2one("ni.observation.type", readonly=True, required=True)
+    sequence = fields.Integer(related="type_id.sequence", readonly=True)
+    category_id = fields.Many2one(related="type_id.category_id", readonly=True)
+    value_type = fields.Selection(
+        [("char", "Char"), ("float", "Float"), ("int", "Integer"), ("code_id", "Code")],
+        readonly=True,
+    )
+    value = fields.Char(readonly=True)
+    value_char = fields.Char(readonly=True)
+    value_int = fields.Integer(group_operator="avg", readonly=True)
+    value_float = fields.Float(group_operator="avg", readonly=True)
+    value_code_id = fields.Many2one("ni.observation.value.code", readonly=True)
+    unit_id = fields.Many2one(related="type_id.unit_id")
+    interpretation_id = fields.Many2one(
+        "ni.observation.interpretation",
+        readonly=True,
+    )
+    is_problem = fields.Boolean(readonly=True)
+    display_class = fields.Selection(
+        related="interpretation_id.display_class",
+    )
+
+    observation_id = fields.Many2one(
+        "ni.observation", compute="_compute_observation", compute_sudo=True
+    )
+
+    history_ids = fields.One2many(related="observation_id.history_ids")
+    history_count = fields.Integer(related="observation_id.history_count")
+
+    def _compute_observation(self):
+        for rec in self:
+            rec.observation_id = self.env["ni.observation"].browse(self.id)
+
+    def action_graph_view(self):
+        self.ensure_one()
+        return self.observation_id.view_graph()
