ef8348d4a83383ddc5eed74839500bc4840c2645
Piruin P. <piruin.p@gmail.com>
2025-08-20 16:39:56 +0700

    [IMP] nirun_presense: remove mock data of 'presense.secret' because noupdate='1' not work as expected

--- nirun_presense/models/ni_patient.py
+++ nirun_presense/models/ni_patient.py
@@ -3,29 +3,34 @@ import pprint
 
 from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
+
 from .presense_token import presense_token_encode
 
+import logging
+
+_logger = logging.getLogger(__name__)
 
 class Patient(models.Model):
     _inherit = "ni.patient"
 
     def action_presense_link(self):
         self.ensure_one()
+        patient_id = self.id
         presense_secrets = (
             self.env["ir.config_parameter"]
             .sudo()
-            .get_param("presense.secret", "fXAhj78KE-sPmGIiMcKSEJ0r?uB6Ko8t=n043tO=HL18XJdkXyarMZuFpGgVWprAURAkHN5GkM1?SQZoRL7r/R4xvMZ02vJM")
+            .get_param("presense.secret", "")
         )
-        access_token = presense_token_encode(presense_secrets, str(self.id))
+        _logger.debug("Creating Patient({}) Link to presense with token({})".format(patient_id, presense_secrets))
+        access_token = presense_token_encode(presense_secrets, str(patient_id))
         presense_url = (
             self.env["ir.config_parameter"]
             .sudo()
-            .get_param("presense.url", "https://dop-uat.airpresense.tech/access_token={}&patient_id={}")
+            .get_param("presense.url", "https://dop-uat.airpresense.tech/access/patient?access_token={}&id={}")
         )
         action = {
             "type": "ir.actions.act_url",
             "target": "new",
-            "url": presense_url.format(access_token, self.id),
+            "url": presense_url.format(access_token, patient_id),
         }
-        pprint.pprint(action)
         return action
