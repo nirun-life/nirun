bc9b7bc7e12fee2b13aadd51fb6dcafdbd2c6485
Piruin P. <piruin.p@gmail.com>
2026-03-09 17:32:55 +0700

    [IMP] ni_observation: remove ni_observation_value_code.type_id use only type_ids

--- ni_observation/models/ni_observation_value_code.py
+++ ni_observation/models/ni_observation_value_code.py
@@ -1,5 +1,5 @@
 #  Copyright (c) 2021 NSTDA
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class ObservationValueCode(models.Model):
@@ -7,7 +7,6 @@ class ObservationValueCode(models.Model):
     _description = "Observation Category"
     _inherit = ["ni.coding"]
 
-    type_id = fields.Many2one("ni.observation.type")
     type_ids = fields.Many2many(
         "ni.observation.type",
         "ni_observation_type_value_code_rel",
@@ -16,9 +15,3 @@ class ObservationValueCode(models.Model):
         required=False,
         ondelete="cascade",
     )
-
-    @api.constrains("type_id", "type_ids")
-    def _check_type_id(self):
-        for rec in self:
-            if rec.type_id and rec.type_id not in rec.type_ids:
-                rec.type_ids = [fields.Command.link(rec.type_id.id)]
