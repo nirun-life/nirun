eed3c3607e62c845cd749ec3e477f7134431f8a4
Nutcha <nutcha.noonmomo@gmail.com>
2026-04-05 11:47:25 +0700

    [ADD] ni_community_care_device: add commu device to update ni_patient_view_form_simp

---
+++ ni_community_care_device/__manifest__.py
@@ -0,0 +1,20 @@
+#  Copyright (c) 2021-2023 NSTDA
+
+{
+    "name": "Device",
+    "version": "16.0.0.3.0",
+    "development_status": "Alpha",
+    "category": "Medical",
+    "author": "NSTDA, Piruin P.",
+    "website": "https://nirun.life/",
+    "license": "LGPL-3",
+    "maintainers": ["piruin"],
+    "depends": ["ni_community_care", "ni_device"],
+    "data": [
+        "views/ni_patient_views.xml",
+    ],
+    "assets": {"web.assets_backend": []},
+    "application": False,
+    "auto_install": False,
+    "installable": True,
+}
